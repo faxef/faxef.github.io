9186905f45a70a2d93028fecee10470b5b5e2995
faxef <dunk-vj_spk@mail.ru>
2017-07-08 19:33:42 +0300

    portfolio filter

--- Portfolio/js/resume.js
+++ Portfolio/js/resume.js
@@ -188,19 +188,6 @@ $( document ).ready(function() {
         }
       });
 	  
-	  
-	  function initialize()
-		{
-		var mapProp = {
-		  center:new google.maps.LatLng(51.508742,-0.120850),
-		  zoom:5,
-		  mapTypeId:google.maps.MapTypeId.ROADMAP
-		  };
-		var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-		}
-
-		google.maps.event.addDomListener(window, 'load', initialize);
-	
 	  /*Javascript for portfolio*/
     var $container = $('#container');
     $container.isotope({
