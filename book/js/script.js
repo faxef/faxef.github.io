e501be9029a53965e3fd588f4c07e3b2de104a63
Faxef <dunk-vj_spk@mail.ru>
2018-02-15 01:25:04 +0300

    fonts

--- book/js/script.js
+++ book/js/script.js
@@ -4,11 +4,26 @@ jQuery(document).ready(function ($) {
 	var email = $('input[name="email"]');
 	var text = $('textarea[name="message"]');
 
+	name.focus(function(){
+		name.css(
+			'border', '1px solid black'
+			)
+	})
+	email.focus(function(){
+		email.css(
+			'border', '1px solid black'
+			)
+	})
+	text.focus(function(){
+		text.css(
+			'border', '1px solid black'
+			)
+	})
+
 	// события при нажатии кнопки отправить полей
 	$('.form-group .formBtn').on('click', function Info () {
-
 		// проверка на пустые поля
-		if(name.val() !="" && email.val() !="" && email.val() !=""){
+		if(name.val() !="" && email.val() !="" && text.val() !=""){
 			var item = $('.info .info__items').append($('<div class="person"></div>'))
 			$('.person').last()
 			.append($('<div class="person__name">' + name.val() + '</div>'))
@@ -31,10 +46,35 @@ jQuery(document).ready(function ($) {
 				.animate({opacity: 1}, 400)
 			})
 		}	
+		else if (name.val() == '') {
+			$('.formBtn').addClass('animated wobble')
+			setInterval(function(){
+				$('.formBtn').removeClass('animated wobble')
+			}, 2000)
+			name.css(
+				'border', '1px solid red'
+				)
+		}else if (email.val() == '') {
+			$('.formBtn').addClass('animated wobble')
+			setInterval(function(){
+				$('.formBtn').removeClass('animated wobble')
+			}, 2000)
+			email.css(
+				'border', '1px solid red'
+				)
+		}else if (text.val() == '') {
+			$('.formBtn').addClass('animated wobble')
+			setInterval(function(){
+				$('.formBtn').removeClass('animated wobble')
+			}, 2000)
+			text.css(
+				'border', '1px solid red'
+				)
+		}
 	})
 	// анимация при закрытии книги
 	$('.container .overlay').on('click',function(event){
-		$('.container .info').css('display', 'none')
+		$('.container .info')
 		.animate({opacity: 0}, 400, function(){
 			$('.container .overlay')
 			.css('visibility', 'hidden')
@@ -47,8 +87,8 @@ jQuery(document).ready(function ($) {
 			})
 		})
 	// стирайние данных
-		name.val('')
-		email.val('')
-		text.val('')
-	})
+	name.val('')
+	email.val('')
+	text.val('')
+})
 })
