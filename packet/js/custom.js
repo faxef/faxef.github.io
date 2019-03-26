870cbd5925497b6f4b941a520aab2980a640458b
faxef <dunk-vj_spk@mail.ru>
2019-03-26 17:17:27 +0300

    product.html

--- packet/js/custom.js
+++ packet/js/custom.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
 		body.removeClass('overlay');
 		body.find('.load_popup').removeClass('show');
 	})
+
+	$('.groups_content_tabs .tab').on('click', function (){
+		$(this).addClass('active').siblings().removeClass('active')
+		$(this).closest('.groups_content').find('.group').removeClass('active').eq($(this).index()).addClass('active');
+	});
+	$('.market .market_title').on('click', function(){
+		$(this).siblings('.market_content').toggleClass('show')
+	})
+	$('.products .product_lists .product').on('click', function(){
+		$(this).toggleClass('selected')
+	})
 })
